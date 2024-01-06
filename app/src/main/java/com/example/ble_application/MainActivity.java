ff48295602408d43a9443806d93aab2c92adcb2d
Mohammadreza <mohammadreza7711@yahoo.com>
2024-01-06 16:54:33 +0330

    Enabling BLE added!

--- app/src/main/java/com/example/ble_application/MainActivity.java
+++ app/src/main/java/com/example/ble_application/MainActivity.java
@@ -1,15 +1,51 @@
 package com.example.ble_application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private BluetoothAdapter BA = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BA = BluetoothAdapter.getDefaultAdapter();
+        checkBTState();
+
+
+    }
+    private void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();}
+    private void checkBTState() {
+        if (BA == null) {
+            errorExit("Error", "Bluetooth not support");
+        }
+        else {
+            if (!BA.isEnabled()) {
+                showToast("...Please Turn Bluetooth ON...");
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    if (Build.VERSION.SDK_INT >= 31) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 100);
+                        return;
+                    }
+                }
+                BA.enable();
+            } else {
+                showToast("Bluetooth is ON!");
+            }
+        }
+    }
+    private void errorExit(String title, String message) {
+        Toast.makeText(getBaseContext(), title + " - " + message, Toast.LENGTH_LONG).show();
+        finish();
     }
 }
 
