161d97a4edd711403b0cc17f183dabab14bbedc8
Mohammadreza <mohammadreza7711@yahoo.com>
2024-09-15 16:44:11 +0330

    VolumeControlPage added!

--- app/src/main/java/com/example/ble_application/VolumePage.java
+++ app/src/main/java/com/example/ble_application/VolumePage.java
@@ -3,26 +3,29 @@ package com.example.ble_application;
 import android.os.Bundle;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class VolumePage extends AppCompatActivity {
 
     private SeekBar volumeSeekBar;
     private TextView currentValueTextView;
-
+    int progressChangedValue = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volume_page);
 
-        //volumeSeekBar = findViewById(R.id.volume_seekbar);
-
+        volumeSeekBar = findViewById(R.id.seekBar);
+        currentValueTextView= findViewById(R.id.value);
 
         // تنظیم مقدار فعلی اسلایدر
         volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                currentValueTextView.setText(String.valueOf(progress));
+                //currentValueTextView.setText(String.valueOf(progress));
+                progressChangedValue = progress;
+                currentValueTextView.setText(String.valueOf(progressChangedValue));
             }
 
             @Override
@@ -33,7 +36,11 @@ public class VolumePage extends AppCompatActivity {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 // کاری انجام نمی‌شود
+                //showToast(String.valueOf(progressChangedValue));
             }
         });
+    }
+    private void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
