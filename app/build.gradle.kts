cacecda91217b6bae7f8aea3dd1233d1985deb38
Mohammadreza <mohammadreza7711@yahoo.com>
2024-10-09 17:12:15 +0330

    UI almost finished

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment:2.7.6")
     implementation("androidx.navigation:navigation-ui:2.7.6")
     implementation ("androidx.viewpager2:viewpager2:1.0.0")
+    implementation ("com.google.android.material:material:1.6.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/ble_application/SoundEnhancer.java
+++ app/src/main/java/com/example/ble_application/SoundEnhancer.java
@@ -3,12 +3,198 @@ package com.example.ble_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 public class SoundEnhancer extends AppCompatActivity {
-
+    Button b1,b2,b3,b4,b5,b6,b7,b8;
+    SeekBar s1,s2,s3;
+    int x=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound_enhancer);
+        b1=findViewById(R.id.button1);
+        b2=findViewById(R.id.button2);
+        b3=findViewById(R.id.button3);
+        b4=findViewById(R.id.speech);
+        b5=findViewById(R.id.clarity);
+        b6=findViewById(R.id.sharp);
+        b7=findViewById(R.id.echo);
+        b8=findViewById(R.id.reset2);
+        s1=findViewById(R.id.seekBar2);
+        s2=findViewById(R.id.seekBar3);
+        s3=findViewById(R.id.seekBar4);
+        s1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                x = progress;
+                if (x==-6)
+                    b1.setText("-6");
+                if (x==-5)
+                    b1.setText("-5");
+                if (x==-4)
+                    b1.setText("-4");
+                if (x==-3)
+                    b1.setText("-3");
+                if (x==-2)
+                    b1.setText("-2");
+                if (x==-1)
+                    b1.setText("-1");
+                if (x==0)
+                    b1.setText("0");
+                if (x==6)
+                    b1.setText("+6");
+                if (x==5)
+                    b1.setText("+5");
+                if (x==4)
+                    b1.setText("+4");
+                if (x==3)
+                    b1.setText("+3");
+                if (x==2)
+                    b1.setText("+2");
+                if (x==1)
+                    b1.setText("+1");
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // کاری انجام نمی‌شود
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // کاری انجام نمی‌شود
+                //showToast(String.valueOf(progressChangedValue));
+            }
+        });
+        s2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                x = progress;
+                if (x==-6)
+                    b2.setText("-6");
+                if (x==-5)
+                    b2.setText("-5");
+                if (x==-4)
+                    b2.setText("-4");
+                if (x==-3)
+                    b2.setText("-3");
+                if (x==-2)
+                    b2.setText("-2");
+                if (x==-1)
+                    b2.setText("-1");
+                if (x==0)
+                    b2.setText("0");
+                if (x==6)
+                    b2.setText("+6");
+                if (x==5)
+                    b2.setText("+5");
+                if (x==4)
+                    b2.setText("+4");
+                if (x==3)
+                    b2.setText("+3");
+                if (x==2)
+                    b2.setText("+2");
+                if (x==1)
+                    b2.setText("+1");
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // کاری انجام نمی‌شود
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // کاری انجام نمی‌شود
+                //showToast(String.valueOf(progressChangedValue));
+            }
+        });
+        s3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                x = progress;
+                if (x==-6)
+                    b3.setText("-6");
+                if (x==-5)
+                    b3.setText("-5");
+                if (x==-4)
+                    b3.setText("-4");
+                if (x==-3)
+                    b3.setText("-3");
+                if (x==-2)
+                    b3.setText("-2");
+                if (x==-1)
+                    b3.setText("-1");
+                if (x==0)
+                    b3.setText("0");
+                if (x==6)
+                    b3.setText("+6");
+                if (x==5)
+                    b3.setText("+5");
+                if (x==4)
+                    b3.setText("+4");
+                if (x==3)
+                    b3.setText("+3");
+                if (x==2)
+                    b3.setText("+2");
+                if (x==1)
+                    b3.setText("+1");
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // کاری انجام نمی‌شود
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // کاری انجام نمی‌شود
+                //showToast(String.valueOf(progressChangedValue));
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                s1.setProgress(0);
+                s2.setProgress(3);
+                s3.setProgress(0);
+            }
+        });
+
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                s1.setProgress(0);
+                s2.setProgress(3);
+                s3.setProgress(3);
+            }
+        });
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                s1.setProgress(0);
+                s2.setProgress(0);
+                s3.setProgress(-2);
+            }
+        });
+        b7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                s1.setProgress(-2);
+                s2.setProgress(0);
+                s3.setProgress(0);
+            }
+        });
+        b8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                s1.setProgress(0);
+                s2.setProgress(0);
+                s3.setProgress(0);
+            }
+        });
     }
 }
